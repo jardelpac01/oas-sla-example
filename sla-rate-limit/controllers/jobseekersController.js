503c96824dd750d7fd82631c0a5231af29102eb1
Jara del Viejo <jardelpac@alum.us.es>
2023-05-12 12:42:27 +0200

    taller l13

---
+++ sla-rate-limit/controllers/jobseekersController.js
@@ -0,0 +1,16 @@
+export function getJobseekers(_, res) {
+    res.send([{
+        type:  "TEAS",
+        year: 2008
+    }]);
+}
+
+
+export function findBytype(_, res) {
+    res.send([{
+        type:  "TEAS",
+        year: 2008
+    }]);
+}
+
+
